16f406e11b6396b45d2c053f4ccd012c12389615
Danie velandia <danielvelandia06@gmail.com>
2026-03-23 01:03:10 -0500

    ayuda para el frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import tareasrouter from "./routers/tareas.js";
 
@@ -7,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use("/tareas", tareasrouter);
 
