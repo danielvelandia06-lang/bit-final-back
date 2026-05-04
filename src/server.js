a34da23b3561f885d18c38c527d4a0c84ab6906d
Danie velandia <danielvelandia06@gmail.com>
2026-05-04 02:20:18 -0500

    despliegue del proyecto final

--- src/server.js
+++ src/server.js
@@ -4,17 +4,29 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import tareasrouter from "./routers/tareas.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/tareas", tareasrouter);
 
+// vamos a hacer la petición para que se muestre nuestro front
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
